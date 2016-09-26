025f20d83a8bd852c8f7f023ee7bbc4505600932
Ho <mh47723@lib-pclem026.lib.utexas.edu>
2016-09-26 16:56:07 -0500

    "Entering Debugging"

--- Lab3/ViewController.swift
+++ Lab3/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    //MAKE Properties
+    @IBOutlet
+    var tableView: UITableView!
+    var items: [String] = ["We", "Heart", "Swift"]
+    
+    /*var one = "\(1)" // "1"
+    var two = "\(1 + 1)" // "2"
+    var name = "iShinta"
+    print("My name is \(name) and I heart Swift")*/
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, numberofRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
+        
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
 }
+
 
